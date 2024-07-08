1e325327711b7564838931c8e8550aa88c9369e7
Christoph Staudt <christoph.staudt@uni-jena.de>
2024-07-08 14:26:36 +0200

    Forward post request

--- tenvexity/functions/api.js
+++ tenvexity/functions/api.js
@@ -1,4 +1,4 @@
-export function onRequest(context) {
+export function onRequest({ request }) {
   /**
    * An object with different URLs to fetch
    * @param {Object} ORIGINS
@@ -6,6 +6,16 @@ export function onRequest(context) {
 
   const url = new URL(request.url);
   url.hostname = 'tenvexity.api.einsum.org';
+
+  // Create a new request based on the original one
+  const init = {
+    body: request.body,
+    headers: request.headers,
+    method: request.method,
+  };
+
+  const newRequest = new Request(url, init);
+
   // If it is, proxy request to that third party origin
-  return fetch(url.toString(), request);
+  return fetch(newRequest);
 }
