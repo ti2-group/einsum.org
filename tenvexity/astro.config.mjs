0943d3ad6780667cea28a6dfe56d21fdeaeda5a0
Christoph Staudt <christoph.staudt@uni-jena.de>
2024-10-14 15:59:59 +0200

    Update config and add math support

--- tenvexity/astro.config.mjs
+++ tenvexity/astro.config.mjs
@@ -1,44 +1,45 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Tenvexity",
-      social: {
-        github: "https://github.com/withastro/starlight",
+      url: 'https://tenvexity.einsum.org',
+      title: 'Tenvexity',
+      logo: {
+        src: './public/favicon.svg',
+        alt: 'Tenvexity',
       },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-      customCss: ["./src/styles.css"],
+      social: {
+        github: 'https://github.com/ti2-group/einsum.org',
+      },
+      customCss: ['./src/styles.css'],
       components: {
-        PageTitle: "./src/components/PageTitle.astro",
+        PageTitle: './src/components/PageTitle.astro',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/ti2-group/einsum.org/edit/main/tenvexity/',
       },
     }),
     tailwind({
       applyBaseStyles: false,
-      configFile: "./tenvexity/tailwind.config.mjs",
+      configFile: './tenvexity/tailwind.config.mjs',
     }),
     react(),
   ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  },
   vite: {
     server: {
       proxy: {
-        "/api": "http://127.0.0.1:5000",
+        '/api': 'http://127.0.0.1:5000',
       },
     },
   },
