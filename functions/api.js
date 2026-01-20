1f854083311e1aa12f83c59dade75c15f5440a55
Christoph Staudt <christoph.staudt@uni-jena.de>
2026-01-20 09:13:50 +0100

    Change proxy domain

--- functions/api.js
+++ functions/api.js
@@ -9,7 +9,7 @@ export function onRequest({ request }) {
 
   const url = new URL(request.url);
   url.protocol = 'http:';
-  url.hostname = `${subdomain}.api.einsum.org`;
+  url.hostname = `${subdomain}.api.ti2.fmi.uni-jena.de`;
 
   // Create a new request based on the original one
   const init = {
