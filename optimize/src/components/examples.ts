68e8a3f8f73ebe6e847628c506473896f1ad29b0
Paul Gerhardt Rump <paul.gerhardt.rump@uni-jena.de>
2026-01-21 12:35:45 +0100

    Optimization: +2 Examples

--- optimize/src/components/examples.ts
+++ optimize/src/components/examples.ts
@@ -2,24 +2,48 @@ export const examples = [
   {
     title: 'Tucker decomposition',
     value: `parameters // Input tensors (training data)
+  T 3      // The tensor we want to decompose
+variables  // Variable tensors (model weights)
+  C 3      // The core of the decomposition
+  u 2, v 2, w 2 // One matrix for each axis
+min        // The objective function
+  norm(T - #(ijk,ai,bj,ck->abc; C, u, v, w))^2`,
+  },
+  {
+    title: 'Tucker decomposition (Robust regression)',
+    value: `parameters // Input tensors (training data)
   T 3     // The tensor we want to decompose
 variables // Variable tensors (model weights)
   C 3     // The core of the decomposition
   u 2, v 2, w 2 // One matrix for each axis
 min       // The objective function
-  norm(T - #(ijk,ai,bj,ck->abc;C,u,v,w))^2`,
+  sum(|T - #(ijk,ai,bj,ck->abc; C, u, v, w)|)`,
   },
   {
     title: 'Non-negative tensor regression (ring network)',
     value: `parameters // Input tensors (training data)
   X 4, y 1 // Features X and labels y
-variables // Variable tensors (model weights)
+variables  // Variable tensors (model weights)
   th0 3, th1 3, th2 3 // Components of theta
-cse       // Reusable (sub)expressions
-  theta=#(zax,xby,ycz->abc;th0,th1,th2)
-min       // The objective function
+cse        // Reusable (sub)expressions
+  theta=#(zax,xby,ycz->abc; th0, th1, th2)
+min        // The objective function
   sum((#(nabc,abc->n;X,theta)-y)^2)
-st        // Optimization constraints
+st         // Optimization constraints
+  theta >= 0`,
+  },
+  {
+    title: 'Non-negative tensor regression (wheel network)',
+    value: `parameters // Input tensors (training data)
+  X 4, y 1 // Features X and labels y
+variables  // Variable tensors (model weights)
+  C 3      // The core of the decomposition
+  th0 4, th1 4, th2 4 // One tensor per axis
+cse        // Reusable (sub)expressions
+  theta=#(ijk,zaix,xbjy,yckz->abc; C, th0, th1, th2)
+min        // The objective function
+  sum((#(nabc,abc->n;X,theta)-y)^2)
+st         // Optimization constraints
   theta >= 0`,
   },
   {
@@ -29,7 +53,7 @@ st        // Optimization constraints
 variables  // Variable tensors
   V 2      // Target matrix
 min        // The objective function
-  #(ji,jk,ki->;V,D-W,V)  // tr(V^T*(D-W)*V)
+  #(ji,jk,ki->; V, D-W, V)  // tr(V^T*(D-W)*V)
 st         // Optimization constraints
   #(ji,jk->ik;V,V) == delta(1) // V^T*V = I
     `,
@@ -38,13 +62,13 @@ st         // Optimization constraints
     title: 'Density estimation (tensor train)',
     value: `parameters // Input tensors (training data)
   D0 2, D1 2, D2 2 // One matrix per feature
-variables // Variable tensors (model weights)
+variables  // Variable tensors (model weights)
   th0 2, th1 3, th2 2 // Cores in the tensor train
-cse       // Common subexpressions
+cse        // Common subexpressions
   theta = #(ax,xby,yc->abc;th0,th1,th2)
-max       // The objective function
-  sum(log(#(abc,an,bn,cn->n;theta,D0,D1,D2)^2))
-st        // Constraints of the optimization
+max        // The objective function
+  sum(log(#(abc,an,bn,cn->n; theta, D0, D1, D2)^2))
+st         // Constraints of the optimization
   ||theta||^2 == 1`,
   },
 ] as { title: string; value: string }[];
